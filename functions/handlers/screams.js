323e90f779f8c8f940c47aab546a37538a50c610
Stanslaus <mstans8@gmail.com>
2020-02-16 00:18:42 +0300

    fetch scream details endpoint with comments

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -44,8 +44,49 @@ const postScream = (req, res) => {
         });
 }
 
+const getScream = (req, res) => {
+    let screamData = {};
+    db
+        .doc(`/screams/${req.params.screamId}`)
+        .get()
+        .then(scream => {
+            if (!scream.exists) {
+                return res.status(404).json({ error: `Scream not found with id: ${req.params.screamId}` })
+            }
+            screamData = scream.data();
+            screamData.screamId = scream.id;
+            return db
+                .collection('comments')
+                .orderBy('createdAt', 'desc')
+                .where('screamId', '==', scream.id)
+                .get();
+        })
+        .then(comments => {
+            screamData.comments = [];
+            comments.forEach(comment => {
+                screamData.comments.push(comment.data());
+            })
+            return res.json(screamData);
+        })
+        .catch(err => {
+            console.error(`Error fetching scream data: ${err}`);
+            return res.status(500).json({ error: `Error fetching scream data: ${err}` });
+        });
+
+}
+
+const deleteScream = (req, res) => {
+
+}
+
 
 router.get('/', getAllScreams);
 router.post('/', FBAuth, postScream);
+router.get('/:screamId', FBAuth, getScream);
+// TODO
+router.delete('/:screamId', FBAuth, deleteScream);
+// TODO like a scream
+// TODO unlike a scream
+// TODO comment on a scream
 
 module.exports = router;
