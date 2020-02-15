c935bf4ab7575619f54b47f0cdbdeae87e73484c
Stanslaus <mstans8@gmail.com>
2020-02-15 14:42:41 +0300

    define structure for user details

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,17 @@
 let db = {
+    users: [
+        {
+            userId: 'HsFa1lGl5OXXPnxHlVDbuM5Ovmq2',
+            email: 'test@gmail.com',
+            handle: 'test',
+            createdAt: '2020-02-13T11:50:05.834Z',
+            imgUrl: 'https://firebasestorage.googleapis.com/v0/b/minsocial-c1acc.appspot.com/o/stans.png?alt=media',
+            bio: 'Hello, I am the first user and welcome to the social site',
+            website: 'https://test.org',
+            location: 'Nairobi, Kenya'
+        }
+
+    ],
     screams: [
         {
             userHandle: "user",
